81e2bc0a6defe38a828b4ca5140979a5a5be43e2
thebluegene <gene.ang92@gmail.com>
2017-09-30 23:41:12 -0700

    v2: Removes references to removed component (from previous commit)

--- src/Index.js
+++ src/Index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './components/App';
-import About from './components/About';
 import Photo from './components/Photo';
-import Film from './components/Film';
 import Blog from './components/Blog';
 import Code from './components/Code';
 
@@ -13,7 +11,6 @@ window.React = React;
 render(
   (<Router history={hashHistory}>
     <Route path="/" component={App}>
-      <Route path="/about" component={About} />
       <Route path="/code" component={Code} />
       <Route path="/photo" component={Photo} />
       <Route path="/photo/:album" component={Photo}/>
