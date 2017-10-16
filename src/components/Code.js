f23d6adf30b2bf8905103567166a99e19d5bae15
thebluegene <gene.ang92@gmail.com>
2017-10-15 22:59:03 -0700

    v2.1: Font and style updates

--- src/components/Code.js
+++ src/components/Code.js
@@ -15,6 +15,12 @@ class Code extends React.Component {
         {
           'title': '8 Years of Photography',
           'date': 'May 2017',
+          'subtitle': 'Data visualization project using EXIF data from all the photos I\'ve posted online',
+          'color': '#004777',
+          'buttonStyle': {
+            transition: '1s all',
+            borderBottom: '1px solid #004777'
+          },
           'styles': {
             fontFamily: 'Fredericka the Great',
             fontSize: '40px'
@@ -23,6 +29,12 @@ class Code extends React.Component {
         }, {
           'title': 'Photo Gallery',
           'date': 'September 2017',
+          'subtitle': 'React photo gallery using Flickr\'s API',
+          'color': '#FF7700',
+          'buttonStyle': {
+            transition: '1s all',
+            borderBottom: '1px solid #FF7700'
+          },
           'styles': {
             fontFamily: 'Suranna',
             fontSize: '40px'
@@ -32,6 +44,11 @@ class Code extends React.Component {
           'title': 'Past Projects',
           'subtitle': '(Coming soon)',
           'date': 'January 2016',
+          'color': '#A30000',
+          'buttonStyle': {
+            transition: '1s all',
+            borderBottom: '1px solid #A30000'
+          },
           'styles': {
             fontFamily: 'Arial',
             fontSize: '40px'
@@ -42,6 +59,27 @@ class Code extends React.Component {
     }
   }
 
+  changeBg(i) {
+    this.state.projectArray[i].buttonStyle = {
+      borderBottom: '1px solid ' + this.state.projectArray[i].color,
+      backgroundColor: this.state.projectArray[i].color,
+      transition: '1s all'
+    }
+    this.setState({
+      projectArray: this.state.projectArray
+    });
+  }
+
+  revertBg(i) {
+    this.state.projectArray[i].buttonStyle = {
+      borderBottom: '1px solid ' + this.state.projectArray[i].color,
+      transition: '1s all'
+    }
+    this.setState({
+      projectArray: this.state.projectArray
+    });
+  }
+
   render() {
     return (
       <div className={this.state.idClass}>
@@ -49,7 +87,7 @@ class Code extends React.Component {
           <div className={this.state.layoutClass + " columns"}>
             {this.state.projectArray.map((data, i) => {
               return (
-                <div key={i} className="code">
+                <div key={i} className="code" style={this.state.idClass ? data.buttonStyle : {}} onMouseLeave={this.revertBg.bind(this, i)} onMouseEnter={this.changeBg.bind(this, i) }>
                   <a href={data.link}>
                     <div className="code__title" style={data.styles}>
                       {data.title}
