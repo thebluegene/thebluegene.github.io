9ee8225d3c7ce4615ea49b39d243df017bde6c7a
thebluegene <gene.ang92@gmail.com>
2017-10-28 18:06:38 -0700

    v2.4: Adds home page transitions. Minor style updates

--- src/components/App.js
+++ src/components/App.js
@@ -6,21 +6,32 @@ import Blog from './Blog';
 import Code from './Code';
 import Social from './Social';
 import {Link} from 'react-router';
-import $ from 'jquery';
 import { Transition } from 'react-transition-group';
+import $ from 'jquery';
 
-const duration = 1000;
+const duration = 400;
 
 const defaultStyle = {
-  transition: `background ${duration}ms ease-in-out`,
-  // background: 'red',
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
 }
 
 const transitionStyles = {
-  // entering: { background: 'red' },
-  // entered: { background: 'blue' },
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+  exiting: {opacity: 0},
+  exited: {opacity: 0}
 };
 
+const linkStyle = {
+  transition: 'all 1s ease-in-out',
+  width: 0
+}
+
+const transitionLinkStyles = {
+  entering: { width: 0 },
+  entered: { width: '100%' }
+}
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -43,69 +54,96 @@ class App extends React.Component {
           image: ''
         }
       ],
-      background: 'default-bg',
-      fadeIn: false
+      animateIn: false
     };
   }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     fadeIn: true
-  //   });
-  // }
-  //
-  // componentDidUpdate(prevProps, prevState) {
-  //   if(prevState.fadeIn == false) {
-  //    this.setState({
-  //      fadeIn: true
-  //    })
-  //  }
-  // }
-  //
-  handleClick() {
-    // this.setState({
-    //   fadeIn: false
-    // });
-  }
+   componentDidMount() {
+     this.setState({
+       animateIn: true
+     });
+   }
+
+   componentDidUpdate(prevProps, prevState) {
+     if(prevState.animateIn == false) {
+       this.setState({
+         animateIn: true
+       })
+     }
+   }
+
+   handleClick() {
+     this.setState({
+       animateIn: false
+     });
+   }
 
   render() {
     if (!this.props.children) {
       return (
         <div>
-          <Transition in={true} timeout={duration}>
-            {(state) => (
-            <div className="home-content page" style={{...defaultStyle, ...transitionStyles[state]}}>
-              <div className="row expanded">
-                <Social />
-                <div className="home-content__main">
-                  <div className="main-title">
-                    <h1>
-                      Gene Ang
-                    </h1>
+          <div className="home-content page">
+            <div className="row expanded">
+              <Transition in={this.state.animateIn} timeout={duration * 3} >
+                {(state) => (
+                  <div style={{...defaultStyle, ...transitionStyles[state]}}>
+                    <Social />
                   </div>
-                  <div className="sub-title">
-                    I'm a <Link onClick={this.handleClick.bind(this)} className="code-link" to="/code">Web Developer</Link> in San Francisco, <br />
-                  who takes a lot of <Link onClick={this.handleClick.bind(this)} className="photo-link" to="/photo">Photos</Link> and makes <Link onClick={this.handleClick.bind(this)} className="film-link" to="/film">Videos</Link> here and there. <br />
-              I'll update my <Link onClick={this.handleClick.bind(this)} className="blog-link" to="/blog">blog</Link> occasionally.
-                  </div>
+                )}
+              </Transition>
+              <div className="home-content__main">
+              <Transition in={this.state.animateIn} timeout={duration} >
+                {(state) => (
+                <div className="main-title" style={{...defaultStyle, ...transitionStyles[state]}}>
+                  <h1>
+                    Gene Ang
+                  </h1>
                 </div>
+                )}
+              </Transition>
+              <Transition in={this.state.animateIn} timeout={duration * 2} >
+                {(state) => (
+                <div className="sub-title" style={{...defaultStyle, ...transitionStyles[state]}}>
+                  I'm a
+                  <Link onClick={ this.handleClick.bind(this) } to="/code">
+                  <span className="link-text">Web Developer</span>
+                  <span style={{...linkStyle, ...transitionLinkStyles[state]}} className="link-text--bg code-link"> </span>
+                  </Link>
+                   in San Francisco, <br /> who takes a lot of
+                   <Link onClick={ this.handleClick.bind(this) } to={{
+                  pathname: "/photo",
+                  state: {
+                    page: "album-list"
+                  }}}>
+                  <span className="link-text">Photos</span>
+                  <span style={{...linkStyle, ...transitionLinkStyles[state]}} className="link-text--bg photo-link"> </span></Link>
+                   and makes
+                   <Link to="/film">
+                   <span className="link-text">Videos</span>
+                  <span style={{...linkStyle, ...transitionLinkStyles[state]}} className="link-text--bg film-link"> </span>
+                   </Link>
+                   here and there. <br />
+                  I'll update my
+                  <Link onClick={ this.handleClick.bind(this) } to="/blog">
+                  <span className="link-text">blog</span>
+                  <span style={{...linkStyle, ...transitionLinkStyles[state]}} className="link-text--bg blog-link"> </span>
+                  </Link>
+                  occasionally.
+                </div>
+                )}
+              </Transition>
               </div>
             </div>
-          )}
-          </Transition>
+          </div>
         </div>
       );
     } else {
       return (
           <div>
             <Nav page={this.props.location.pathname} />
-              <Transition in={this.state.fadeIn} timeout={duration}>
-                {(state) => (
-                  <div style={{...defaultStyle, ...transitionStyles[state]}}>
+                  <div>
                     { this.props.children }
                   </div>
-                )}
-            </Transition>
             <div className="hide-for-small-only">
               <Social />
             </div>
